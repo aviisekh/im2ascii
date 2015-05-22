5076aa7346cebbdc79d21a22ee26d5f4daa71d65
Abhishek <aviisekh@gmail.com>
2015-05-22 20:47:58 +0545

    new console program and updated readme

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # im2ascii
+*Convert Your image into an ascii image*
+
+two different outputs are generated
+* text output
+* image output
+
+#### Modules Needed
+* imghdr  
+* Pillow  
+* aalib
+
+To convert a image file to a ASCII run following command.
+
+python ascii_image.py image_name 4 15
+
+“4” is the density of ASCII characters.
+
+“15” is the font size.
+
--- ascii_image.py
+++ ascii_image.py
@@ -1,9 +1,6 @@
 #!/usr/bin/env python
 
-#import cv2
 import sys
-
-
 
 import aalib
 import Image
@@ -15,13 +12,8 @@ from PIL import Image
 from PIL import ImageDraw
 
 import imghdr
-
 import ImageOps
 
-
-
-
-#sys.dont_write_bytecode = True
 imagefilename=str(sys.argv[1]).strip()
 
 #These two variables control the resolution and font size.  They are interconnected.
@@ -42,96 +34,72 @@ font = ImageFont.truetype("/usr/share/fonts/truetype/freefont/FreeMonoBold.ttf",
 
 
 def create_ascii_image():
-
-	def make_text():
-		im = Image.open(imagefilename)
-		image_width, image_height = im.size
-		print os.path.splitext(imagefilename)
-
+	global name
+	name,typ=imagefilename.split(".")
 	
-		#aalib_screen_width= int(image_width/24.9)*img_new_magnitude
-		#aalib_screen_height= int(image_height/41.39)*img_new_magnitude
+	global image_width, image_height
+	image_width, image_height = Image.open(imagefilename).size
 	
-		aalib_screen_width= 80#image_width
-		aalib_screen_height= 40#image_height
-
+	def make_screen_and_image(aalib_screen_width,aalib_screen_height):
 		screen = aalib.AsciiScreen(width=aalib_screen_width, height=aalib_screen_height )
-
-
 		im= Image.open(imagefilename).convert("L").resize(screen.virtual_size)
-		print im.size
+		return screen,im
 	
+	
+
+	def  make_img():
+		
+		aalib_screen_width= int(image_width/30)*img_new_magnitude
+		aalib_screen_height= int(image_height/45)*img_new_magnitude
+		
+		screen,im=make_screen_and_image(aalib_screen_width,aalib_screen_height)
 		screen.put_image((0,0), im)
-		p=open("new.txt","w")
-		q=screen.render()
-		#print q
-		p.write(q)
 
-	def make_img():
-		print "Make Image"
+		y = 0
 	
-	def make_gray():
-		print "Make gray"
+		how_many_rows = len ( screen.render().splitlines() ) 
 
-			
-	choice=input("Enter the choice:\n1. Create text\n2. Create Image\n3. Create Grayscale\n: ")
+		new_img_width, font_size = font.getsize (screen.render().splitlines()[0])	
 
-	options = {1:make_text,2:make_img,3:make_gray,}
-	options[choice]()
-
-	'''
-	im = Image.open(imagefilename)
-	image_width, image_height = im.size
-
+		img=Image.new('RGB', (new_img_width, how_many_rows*font_size),(120,20,20))
+		draw = ImageDraw.Draw(img)
 	
-	#aalib_screen_width= int(image_width/24.9)*img_new_magnitude
-	#aalib_screen_height= int(image_height/41.39)*img_new_magnitude
-	
-	aalib_screen_width= 80#image_width
-	aalib_screen_height= 40#image_height
-
-	screen = aalib.AsciiScreen(width=aalib_screen_width, height=aalib_screen_height )
-
-
-	im= Image.open(imagefilename).convert("L").resize(screen.virtual_size)
-	print im.size
-	
-	screen.put_image((0,0), im)
-	p=open("new.txt","w")
-	q=screen.render()
-	#print q
-	p.write(q)
-	'''
-	
-	'''	
-	y = 0
-	
-	how_many_rows = len ( screen.render().splitlines() ) 
-
-	new_img_width, font_size = font.getsize (screen.render().splitlines()[0])
-	
-
-	img=Image.new('RGB', (new_img_width, how_many_rows*font_size),(120,20,20))
-	draw = ImageDraw.Draw(img)
-	
-	for lines in screen.render().splitlines():
-		draw.text( (0,y), lines, (255,255,0),font=font )
-		y = y + font_size
+		for lines in screen.render().splitlines():
+			draw.text( (0,y), lines, (255,255,0),font=font )
+			y = y + font_size
 		
 
-	imagefit = ImageOps.fit(img, (image_width, image_height), Image.ANTIALIAS)
+		imagefit = ImageOps.fit(img, (image_width, image_height), Image.ANTIALIAS)
+		return imagefit
 
-	grey_img=ImageOps.grayscale(imagefit)
 	
-	#imagefit.save("ascii_photo.png", "PNG")
-	grey_img.save("ascii_photo.png", "PNG")
-	img_new = cv2.imread('ascii_photo.png',0)
-	a,thresh=cv2.threshold(img_new, 127,255,cv2.THRESH_BINARY) 
-	cv2.imwrite('messigray.png',thresh)
-	#ret,thresh1 = cv2.threshold(img,127,255,cv2.THRESH_BINARY)
-	'''
 
+	def make_text():
+		screen,im=make_screen_and_image(80,40)
+	
+		screen.put_image((0,0), im)
+		
+		p=open(name+".txt","w")
+		q=screen.render()
+		p.write(q)
+		p.close()
 
+	def norm():
+		image=make_img()
+		image.save(name+"_converted.jpg", "JPEG")
+
+	def gray():
+		image=make_img()
+		gray_img=ImageOps.grayscale(image)
+		gray_img.save(name+"_converted_gray.jpg", "JPEG")
+		
+	#Switch Statement	
+	choice=input("Enter the choice:\n1. Create text\n2. Create Image\n3. Create Grayscale\n: ")
+
+	options = {1:make_text,2:norm,3:gray,}
+	options[choice]()
+
+	
 def test_if_image():
 
 	if imghdr.what(imagefilename) != None :
@@ -140,6 +108,5 @@ def test_if_image():
 	else: 
 		print ("This is probably not a image.")
 		exit()
-
 
 test_if_image()
